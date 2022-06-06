3aa3c55ac559afed4a183fd2f6d59b6636f96883
Blessing Ubogu <blessingubogu@Blessings-MacBook-Air.local>
2022-06-06 13:44:37 +0100

    exercise 2 - price display using % format

--- Clock.java
+++ Clock.java
@@ -2,12 +2,15 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 class Clock {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public static void main (String[] args) {
         LocalTime currentTime = LocalTime.now();
         DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("HH:mm");
         
         String formattedTime = currentTime.format(myFormatObj);
-        System.out.println("The current time is " + formattedTime );
+        System.out.println("The current" + ANSI_RED + " time" + ANSI_RESET + " is " + formattedTime );
     }
     
 }
---
+++ PriceDisplay.java
@@ -0,0 +1,9 @@
+class PriceDisplay {
+    public static void main(String[] args) {
+        String itemName = "Coffee Machine";
+        Double price = 15.99;
+
+        System.out.printf("The price of the item " + itemName + " is %.2f%n ", price);
+
+    }
+}
