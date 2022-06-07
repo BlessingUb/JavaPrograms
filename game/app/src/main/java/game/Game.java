0c390b2f461240a964eba7176e7f6734ede8d463
Blessing Ubogu <blessingubogu@Blessings-MacBook-Air.local>
2022-06-07 17:20:02 +0100

    first test passed

---
+++ game/app/src/main/java/game/Game.java
@@ -0,0 +1,9 @@
+package game;
+
+public class Game {
+
+    public String getWordToGuess() {
+        return "b____";
+    }
+
+}
---
+++ game/app/src/test/java/game/GameTest.java
@@ -0,0 +1,13 @@
+package game;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class GameTest {
+    @Test 
+    public void testGetsWordToGuess(){
+        Game game = new Game();
+        assertEquals(game.getWordToGuess(), "b____");
+    }
+}
